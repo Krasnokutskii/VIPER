d5875ea97a0aa37ea7d1f6e75451eab76234dde3
Krasnokutskii Iaroslav <Yroslavkr1995@gmail.com>
2022-07-30 21:11:19 +0300

    Initial Commit

---
+++ Viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper
+//
+//  Created by Yaroslav Krasnokutskiy on 30.7.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
